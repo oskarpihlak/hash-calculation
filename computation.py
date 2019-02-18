a1289c094a06263cce977a7cb7736499d6f1153b
oskarp <Oskra.Pihlak@nortal.com>
2019-02-19 00:09:01 +0200

    ads

--- computation.py
+++ computation.py
@@ -1,6 +1,4 @@
-# multiproc_test.py
 import hashlib
-import json
 import multiprocessing
 import pprint
 import re
@@ -9,6 +7,7 @@ from collections import namedtuple
 import os
 import keyboard as keyboard
 import csv
+import sqlite3
 
 now = time.time()
 
@@ -17,6 +16,8 @@ current = 'fadaa78054ac10b5967cd0dd8d7886029a2f4e98ca378a71a5621b42a6126dd3'
 nonce = ''
 prog = re.compile(r"^0+")
 largest_zero_count = 0
+cores = 8
+database = []
 
 
 class bcolors:
@@ -30,48 +31,6 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-cores = 8
-if not os.path.exists('hash_data.csv'):
-    def get_computation_range(i):
-        return (((16 ** 32) // cores) * i) - 1
-
-
-    computation_ranges = {"1": {
-        "unit":1,
-        "start": 0,
-        "end": get_computation_range(1),
-        "largest_zero_count": 0,
-        "best_nonce": '',
-        "best_hash": ''
-    }}
-    for i in range(2, 9):
-        computation_ranges[i] = {
-            "unit": i,
-            "start": get_computation_range(i - 1),
-            "end": get_computation_range(i),
-            "largest_zero_count": 0,
-            "best_nonce": '',
-            "best_hash": ''
-        }
-else:
-    computation_ranges = {}
-    with open('hash_data.csv', newline='\n') as csvfile:
-        hash_data = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in hash_data:
-            computation_ranges[row[0]] = {
-                "unit": row[0],
-                "largest_zero_count": row[1],
-                "best_nonce": row[2],
-                "best_hash": row[3],
-                "start": row[4],
-                "end": row[5]
-            }
-
-    f = open('hash_data.csv', 'w+')
-    f.truncate(0)
-    f.close()
-
-    pprint.pprint(computation_ranges)
 def to_hms(seconds):
     st_time = namedtuple('time', 'hours minutes seconds')
     minutes, seconds = divmod(seconds, 60)
@@ -80,15 +39,16 @@ def to_hms(seconds):
 
 
 def print_time(computation):
+    print(computation)
     global largest_zero_count
     global best_hash
     global best_nonce
 
-    unit = computation['unit']
-    largest_zero_count = int(computation['largest_zero_count'])
-    best_hash = computation['best_hash']
-    best_nonce = computation['best_nonce']
-    start, end = int(computation['start']), int(computation['end'])
+    id = computation[0]
+    largest_zero_count = int(computation[1])
+    best_hash = computation[3]
+    best_nonce = computation[2]
+    start, end = int(computation[4]), int(computation[5])
 
     for i in range(start, end):
         nonce = "{:032x}".format(i)
@@ -97,7 +57,7 @@ def print_time(computation):
         if m:
             if keyboard.is_pressed('q'):
                 print(
-                    f'Proccess {bcolors.FAIL}{unit}{bcolors.ENDC}: {bcolors.OKGREEN}finished - {largest_zero_count} zeroes{bcolors.ENDC}\n'
+                    f'Proccess {bcolors.FAIL}{id}{bcolors.ENDC}: {bcolors.OKGREEN}finished - {largest_zero_count} zeroes{bcolors.ENDC}\n'
                     f'iteration:  {bcolors.OKGREEN}{i}{bcolors.ENDC}\n'
                     f'currentHash:{bcolors.OKGREEN}{"{:032x}".format(i)}{bcolors.ENDC}\n'
                     f'best hash   {bcolors.OKGREEN}{largest_zero_count} zeroes: {best_hash}{bcolors.ENDC}\n'
@@ -105,9 +65,18 @@ def print_time(computation):
                     f'end:        {bcolors.OKGREEN}{end}{bcolors.ENDC}\n'
                     f'end hash    {bcolors.OKGREEN}{"{:032x}".format(end)}{bcolors.ENDC}\n\n\n'
                 )
-                myCsvRow = f'{unit},{largest_zero_count},{best_nonce},{best_hash},{i},{end}\n'
-                with open('hash_data.csv', 'a') as fd:
-                    fd.write(myCsvRow)
+                conn = sqlite3.connect('hash_data.db')
+                c = conn.cursor()
+                update_string = '''UPDATE hash_data SET largest_zero_count={largest_zero_count}, best_hash={best_hash}, start={start}, ending={ending} WHERE  id={id};'''.format(
+                    largest_zero_count=largest_zero_count,
+                    best_hash=str(best_hash),
+                    best_nonce=str(best_nonce),
+                    start=str(start),
+                    ending=str(end),
+                    id=id)
+                with conn:
+                    c.execute(update_string)
+
                 break
             if m.end() > largest_zero_count:
                 largest_zero_count = m.end()
@@ -115,7 +84,7 @@ def print_time(computation):
                 best_nonce = nonce
                 countdown = to_hms(int(time.time() - now))
                 hash_time = f"{bcolors.OKGREEN}{countdown.hours} hour(s) {countdown.minutes} minute(s) {countdown.seconds} second(s){bcolors.ENDC}"
-                status_text = f'\n{hash_time} - Process {bcolors.OKGREEN}{unit}{bcolors.ENDC}' \
+                status_text = f'\n{hash_time} - Process {bcolors.OKGREEN}{id}{bcolors.ENDC}' \
                     f'\nNumber of 0s: {bcolors.FAIL}{largest_zero_count}{bcolors.ENDC}' \
                     f'\nHash:         {bcolors.OKBLUE}{best_hash}{bcolors.ENDC}' \
                     f'\nNonce:        {bcolors.OKBLUE}{best_nonce}{bcolors.ENDC}' \
@@ -124,10 +93,45 @@ def print_time(computation):
 
 
 if __name__ == "__main__":
+
+    conn = sqlite3.connect('hash_data.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS hash_data(
+    id numeric not null primary key , 
+    largest_zero_count numeric ,
+    best_nonce varchar ,
+    best_hash varchar,
+    start varchar(255) ,
+    ending varchar(255)
+    )''')
+    with conn:
+        c.execute("SELECT * FROM hash_data")
+        if len(c.fetchall()) < 8:
+            def hash_insert(id, largest_zero_count, best_nounce, best_hash, start, ending):
+                query = '''INSERT INTO hash_data VALUES ({id},{largest_zero_count},'{best_nounce}','{best_hash}','{start}','{ending}')''' \
+                    .format(id=id, largest_zero_count=largest_zero_count, best_nounce=best_nounce, best_hash=best_hash,
+                            start=start, ending=ending)
+                print(query)
+                c.execute(query)
+
+
+            def get_computation_range(i):
+                l = (((16 ** 32) // 8) * i) - 1
+                print(l, str(l))
+                return l
+
+
+            hash_insert(1, 0, '', '', '0', str(get_computation_range(1)))
+            for i in range(2, 9):
+                hash_insert(i, 0, '', '', str(get_computation_range(i - 1)), str(get_computation_range(i)))
+
+    with conn:
+        c.execute("SELECT * FROM hash_data")
+        database = c.fetchall()
+
     jobs = []
-    for range in computation_ranges:
-        print('lolq',computation_ranges[range])
-        process = multiprocessing.Process(target=print_time, args=(computation_ranges[range],))
+    for core in database:
+        process = multiprocessing.Process(target=print_time, args=(core,))
         jobs.append(process)
 
     # Start the processes (i.e. calculate the random number lists)
